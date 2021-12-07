333dadd436e7a3fc0a7e47e9c7b5819fe658f247
Wheaties466 <Mwheat42289@gmail.com>
2021-12-07 09:12:54 -0500

    Create download_plex_update.sh
    add in your plex download url with your plex token and you should be good to automate this update.

---
+++ download_plex_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PLEX_URL='https://plex.tv/downloads/latest/5?channel=16&build=linux-x86_64&distro=debian&X-Plex-Token=XXXXXXXXXXXXXXXXXXXX'
+#NAME=$(curl $PLEX_URL -v 2>&1 | grep -i location | tail -n 1 | cut -d" " -f3)
+
+NAME=${NAME%$'\r'}
+
+curl -L -O "$NAME"
+
+FILE=$(echo $NAME | cut -d'/' -f 7)
+echo $FILE
+echo "Installing the new plex version in 10 seconds. CTRL + C to stop"
+sleep 10
+dpkg -i $FILE
